dff0651e3ac1d2c981f5cc343bb3621130883fa4
Charles White <charles.white988@gmail.com>
2023-11-17 14:44:02 -1000

    still troubleshooting the 'xticks' error. The backend doesn't seem to matter.

--- files/examples/RunExamples.sh
+++ files/examples/RunExamples.sh
@@ -2,14 +2,14 @@
 declare -A test_cirs
 # test_cirs is a dictionary with the directory as a key and the value as the run file
 test_cirs["balancedCircuits"]="balancedCircuitsProject.py"
-test_cirs["CSstage"]="CSresnoise.py"
+#test_cirs["CSstage"]="CSresnoise.py"
 test_cirs["FilterDesign"]="LR4project.py"
 test_cirs["myFirstRCnetwork"]="myFirstRCnetwork.py"
 test_cirs["noiseFigure"]="noiseFigures.py"
 #This will make sure no plots are shown:
 mkdir allhtml
 cp Template.rst ExampleCirs.rst
-export MPLBACKEND=SVG #For plotting with Matplotlib on the 'SVG' backend
+export MPLBACKEND=svg #For plotting with Matplotlib on the 'SVG' backend
 for key in "${!test_cirs[@]}"; do
     cd $key
     python ${test_cirs[$key]}
