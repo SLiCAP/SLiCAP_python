dfd425342d1b44c198ac49e967c17988e333ea69
anton montagne <anton@montagne.nl>
2024-02-18 14:15:50 +0100

    Windows install searches for KiCad, Inkscape, LTspice

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ Created on Tue Sep 29 09:28:49 2020
 
 @author: luc_e
 
-Modified by Anton 1June 11 2023:
-Updated to version 1.6.0
+Modified by Anton January 27 2024:
+Updated to version 1.8.0
 """
 
 import os, shutil
@@ -15,9 +15,9 @@ from setuptools.command.install import install
 import platform
 import subprocess
 import re
-from shutil import copy
 if platform.system() == 'Windows':
     import win32api
+    import windows_tools.installed_software as wi
 
 INSTALLVERSION="2.0.1"
 
@@ -26,10 +26,14 @@ class InstallWrapper(install):
     Provides a install wrapper for SLiCAP.
     Contains private functions that are to be run.
     """
-    _LTSpice_cmd = "None"
-    _SLiCAP_version = None
+    _ltspice_cmd      = 'None'
+    _kicad_cmd        = 'None'
+    _inkscape_cmd     = 'None'
+    _gnetlist_cmd     = 'None'
+    _ngspice_cmd      = 'None'
+    _SLiCAP_version   = None
     _library_location = None
-    _doc_location = None
+    _doc_location     = None
     _install_location = None
 
     def run(self):
@@ -40,26 +44,138 @@ class InstallWrapper(install):
         -------
         None.
         """
-        self._set_ltspice_command()
+        package_list = ['LTspice', 'Inkscape', 'KiCad', 'gEDA', 'NGspice']
+        self._set_commands(package_list)
         self._set_version_config()
         self._set_install_location()
         self._gen_config_file()
         install.run(self)
 
-    def _set_ltspice_command(self):
+    def _find_installed_windows_software(self, package_list):
         """
-        Determining the LTSPICE command
-        This contains two flows:
-            Windows -   Searches the program files dir (e.g. C:\Program Files) for the LTSpice Command
-            Linux   -   Searches the wine drive directory for the LTSpice command
+        Searches for installed packages from the package list
+
+        Returns
+        -------
+        Dictionary with key-value pairs: key = package name, value = command
         """
-        self._LTSpice_cmd = " "
-        print("Acquiring LTSpice Command")
-        self._find_ltspice()
-        if self._LTSpice_cmd != " ":
-            print("Found the LTSpice directory: ", self._LTSpice_cmd)
+        commands = {}
+        search_list = []
+        software_list = wi.get_installed_software()
+        for dct in software_list:
+            name = dct["name"]
+            if len(name) > 1:
+                name = name.split()[0]
+                if name in package_list:
+                    search_list.append(name)
+        if len(search_list) > 0:
+            print("\nSearching installed software, this can take some time!")
+            y_n = input("\nDo you have NGspice installed? [y/n] >>> ").lower()[0]
+            while y_n != 'y' and y_n != 'n':
+                y_n = input("\nPlease enter 'y' for 'yes' or 'n' for 'no' >>> ").lower()[0]
+            if y_n == 'y':
+                search_list.append('NGspice')
+            print('')
+            for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
+                for root,dirs,files in os.walk(drive):
+                    for name in dirs:
+                        for package in search_list:
+                            if package not in commands.keys():
+                                if package == 'LTspice':
+                                    if re.match('LT(S|s)pice*', name, flags=0):
+                                        if os.path.exists(os.path.join(root,name,'XVIIx64.exe')):
+                                            print("LTSpice command set as:", os.path.join(root,name,'XVIIx64.exe'))
+                                            commands[package] = os.path.join(root,name,'XVIIx64.exe')
+                                        elif  os.path.exists(os.path.join(root,name,'LTspice.exe')):
+                                            print("LTSpice command set as:", os.path.join(root,name,'LTspice.exe'))
+                                            commands[package] = os.path.join(root,name,'LTspice.exe')
+                                        elif  os.path.exists(os.path.join(root,name,'ltspice.exe')):
+                                            print("LTSpice command set as:", os.path.join(root,name,'ltspice.exe'))
+                                            commands[package] = os.path.join(root,name,'ltspice.exe')
+                                if package == 'Inkscape':
+                                    if re.match('Inkscape', name, flags=0):
+                                        file_name = os.path.join(root, name,'bin','inkscape.exe')
+                                        if os.path.exists(file_name):
+                                            print("Inkscape command set as:", os.path.join(root,name,'inkscape.exe'))
+                                            commands[package] = file_name
+                                if package == 'KiCad':
+                                    if re.match('KiCad', name, flags=0):
+                                        version=os.listdir(os.path.join(root, name))[0]
+                                        file_name = os.path.join(root, name, version, 'bin','kicad-cli.exe')
+                                        if os.path.exists(file_name):
+                                            print("KiCad command set as:", os.path.join(root,name,'kicad-cli.exe'))
+                                            commands[package] = file_name
+                                if package == 'gEDA':
+                                    if re.match('gEDA', name, flags=0):
+                                        file_name = os.path.join(root, name, 'gEDA', 'bin' ,'gnetlist.exe')
+                                        if os.path.exists(file_name):
+                                            print("gnetlist command set as:", os.path.join(root,name,'gnetlist.exe'))
+                                            commands[package] = file_name
+                                if package == 'NGspice':
+                                    if re.match('Spice64', name, flags=0):
+                                        file_name = os.path.join(root, name, 'bin' ,'ngspice.exe')
+                                        if os.path.exists(file_name):
+                                            print("NGspice command set as:", os.path.join(root,name,'ngspice.exe'))
+                                            commands[package] = file_name
+                            if len(list(commands.keys())) == len(search_list):
+                                return commands
+        return(commands)
+
+    def _find_LTspice_wine(self):
+        """
+        Searches for LTspice under Linux or MacOS
+
+        Returns
+        -------
+        LTspice command
+        """
+        home = expanduser("~")
+        drives = [os.path.join(home, '.wine', 'drive_c')]
+        for drive in drives:
+            drive = os.path.join(drive, 'Program Files')
+            for root, dirs, files in os.walk(drive, topdown=True):
+                for name in dirs:
+                    if re.match('LT(S|s)pice*', name, flags=0):
+                        if os.path.exists(os.path.join(root,name,'XVIIx64.exe')):
+                            cmd = os.path.join(root,name,'XVIIx64.exe')
+                        elif  os.path.exists(os.path.join(root,name,'LTspice.exe')):
+                            cmd = os.path.join(root,name,'LTspice.exe')
+                        elif  os.path.exists(os.path.join(root,name,'ltspice.exe')):
+                            cmd = os.path.join(root,name,'ltspice.exe')
+                        return cmd
+        return 'None'
+
+    def _set_commands(self, package_list):
+        """
+        Sets the commands for external packages
+        """
+        pltfrm = platform.system()
+        if pltfrm == 'Windows':
+            commands = self._find_installed_windows_software(package_list)
+            for package in package_list:
+                if package in commands.keys():
+                    if package == 'LTspice':
+                        self._ltpice_cmd = commands[package]
+                    elif package == 'Inkscape':
+                        self._inkscape_cmd = commands[package]
+                    elif package == 'KiCad':
+                        self._kicad_cmd = commands[package]
+                    elif package == 'gEDA':
+                        self._gnetlist_cmd = commands[package]
+                    elif package == 'NGspice':
+                        self._gnetlist_cmd = commands[package]
+        elif pltfrm == 'Linux':
+            self._ltspice_cmd  =  self._find_LTspice_wine()
+            self._inkscape_cmd = 'inkscape'
+            self._kicad_cmd    = 'kicad-cli'
+            self._gnetlist_cmd = 'gnetlist'
+            self._ngspice_cmd  = 'ngspice'
         else:
-            print("Could not find LTspice command, you need to edit the configuration file manually!")
+            self._ltspice_cmd  =  self._find_LTspice_wine()
+            self._inkscape_cmd = 'inkscape'
+            self._kicad_cmd    = '/Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli'
+            self._gnetlist_cmd = 'gnetlist'
+            self._ngspice_cmd  = 'ngspice'
 
     def _set_version_config(self):
         """
@@ -121,37 +237,16 @@ class InstallWrapper(install):
             txt = txt.replace("$SYSINSTALL", ' ')
             txt = txt.replace("$USERPATH", self.slicap_home)
             txt = txt.replace("$LIBCOREPATH", self._library_location)
-            txt = txt.replace("$LTSPICE", self._LTSpice_cmd)
+            txt = txt.replace("$LTSPICE", self._ltspice_cmd)
+            txt = txt.replace("$NGSPICE", self._ngspice_cmd)
+            txt = txt.replace("$KICAD", self._kicad_cmd)
+            txt = txt.replace("$INKSCAPE", self._inkscape_cmd)
             txt = txt.replace("$DOCPATH", self._doc_location)
             fi = open(filetarg, 'w')
             fi.write(txt)
             fi.close()
         print("Created config file: ", filetarg)
 
-    def _find_ltspice(self):
-        """
-        Searches for the LTspice command under windows. If found it sets
-        self._LTSpice_cmd.
-
-        Returns
-        -------
-        None.
-        """
-        if platform.system() == 'Windows':
-            drives = win32api.GetLogicalDriveStrings().split('\000')[:-1]
-        else:
-            # Assuming default 'wine' installation
-            home = expanduser("~")
-            drives = [os.path.join(home, '.wine', 'drive_c')]
-        for drive in drives:
-            drive = os.path.join(drive, 'Program Files')
-            for root, dirs, files in os.walk(drive, topdown=True):
-                for name in dirs:
-                    if re.match('LT(S|s)pice*', name, flags=0):
-                        path = os.path.join(root,name,'XVIIx64.exe')
-                        print("LTSpice command set as:", path)
-                        self._LTSpice_cmd = path
-                        return
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -164,7 +259,7 @@ setuptools.setup(
     description="Symbolic Linear Circuit Analysis Program",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Lenty/SLiCAP_python/",
+    url="https://github.com/SLiCAP/SLiCAP_python/",
     packages=setuptools.find_packages(),
     cmdclass={'install': InstallWrapper},
     classifiers=[
