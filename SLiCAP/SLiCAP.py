c4965b1d30bfe580b435c8b6965657f94236d347
Anton Montagne <anton@montane.nl>
2024-12-01 22:08:09 +0100

    Updated jupyter notebook interface

--- SLiCAP/SLiCAP.py
+++ SLiCAP/SLiCAP.py
@@ -29,11 +29,14 @@ from SLiCAP.SLiCAPltspice import runLTspice
 from SLiCAP.SLiCAPshell import *
 from SLiCAP.SLiCAPhtml import *
 from SLiCAP.SLiCAPhtml import _startHTML
-from SLiCAP.SLiCAPnotebook import *
+from IPython.core.display import HTML
 
 # Increase width for display of numpy arrays:
 np.set_printoptions(edgeitems=30, linewidth=1000,
     formatter=dict(float=lambda x: "%11.4e" % x))
+
+def SVG(fileName, width=400):
+    return HTML('<img src="%s" width="%spx">'%(fileName, str(width)))
 
 def Help():
     """
