b30ce5c0d9190806fc0b311745d6c1089506094a
Anton Montagne <anton@montane.nl>
2024-12-04 13:05:27 +0100

    Version 3.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 import setuptools
 from setuptools.command.install import install
 
-INSTALLVERSION="3.1.0"
+INSTALLVERSION="3.1.1"
 
 class InstallWrapper(install):
     """
